2ec91f3a2c6477101b87f31fdf3fc819d4fd666e
RaimundoGallino <raigallino0@gmail.com>
2021-02-03 18:54:16 +0000

    getting to know how it works C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void) 
+{
+printf("Size of an char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long : %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+return (0);
+}
