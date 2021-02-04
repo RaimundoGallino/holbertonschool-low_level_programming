1cbf19be91252a0a848f2c5ab1fdf6eea1856a83
RaimundoGallino <raigallino0@gmail.com>
2021-02-04 20:51:07 +0000

    7 first try

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* your code goes there */
+	char ch = 'z';
+
+	for (; ch <= 'a' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
