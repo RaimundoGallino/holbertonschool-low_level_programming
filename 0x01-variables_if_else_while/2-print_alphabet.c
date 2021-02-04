32a4b1c6aeb54f9ebec5ac94a0bb3f8aa27714a5
RaimundoGallino <raigallino0@gmail.com>
2021-02-04 18:26:09 +0000

    printing alphabet with a for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* your code goes there */
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+
+return (0);
+}
