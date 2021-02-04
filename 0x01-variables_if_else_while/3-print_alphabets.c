fba9335d24516c64d98a0289ce974d1e9a6a9276
RaimundoGallino <raigallino0@gmail.com>
2021-02-04 18:55:45 +0000

    Correction big and lowe case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* your code goes there */
+	char ch = 'a';
+	char ch1 = 'A';
+	for (; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (; ch1 <= 'Z' ; ch1++)
+        {
+                putchar(ch);
+        }
+	putchar('\n');
+
+	return (0);
+}
