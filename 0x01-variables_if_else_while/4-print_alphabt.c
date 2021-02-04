dd55a4edc8b2c6e9ce69663b295ac49c4cb75b77
RaimundoGallino <raigallino0@gmail.com>
2021-02-04 20:18:37 +0000

    4 correction

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* your code goes there */
+	char ch = 'a';
+
+	for (; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+		if (ch = 'q' || ch = 'e')
+		{
+		}
+		else
+		{
+			putchar(ch);
+		}
+	putchar('\n');
+
+	return (0);
+}
