1269199c38c02cece697343157d6282fedd1bbe3
RaimundoGallino <raigallino0@gmail.com>
2021-02-04 20:43:47 +0000

    correction 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* your code goes there */
+	char n;
+
+	for (n = '0'; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
