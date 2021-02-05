404bcce33a4ee054f1589aab5f21e4f496bb4b94
RaimundoGallino <raigallino0@gmail.com>
2021-02-05 02:03:55 +0000

    9-compination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* your code goes there */
+	int n;
+
+	for (n = '0'; n <= '9' ; n++)
+	{
+		putchar(n);
+		if (n < 9)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
