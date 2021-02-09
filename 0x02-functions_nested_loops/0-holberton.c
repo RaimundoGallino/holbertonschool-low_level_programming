32ff4b34297eb8377612884db347bbf83407e94e
RaimundoGallino <raigallino0@gmail.com>
2021-02-09 12:09:16 +0000

    cleaning commentaries

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - writes the character c to stdout
+ * The character to print
  *
- * Return: On success 1.
+ * Return: On success 0.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
