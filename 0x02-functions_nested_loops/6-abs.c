646f27314296b3c3ea58242febe05c406d0d1ab7
RaimundoGallino <raigallino0@gmail.com>
2021-02-09 01:42:32 +0000

    absolute-value-funtion

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+	        return (-n);
+	}
+	else
+	{
+	        return (n);
+	}
+}
