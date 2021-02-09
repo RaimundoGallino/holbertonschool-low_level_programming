f7815202ddb72649c47287c7136650099e9a4e70
RaimundoGallino <raigallino0@gmail.com>
2021-02-09 18:04:48 +0000

    8_24hs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * jack_bauer - print the last digit of a number
+ *
+ *
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void jack_bauer(void)
+{
+	int n1, n2, n3, n4;
+
+
+	for (n1 = '0'; n1 <= '2'; n1++)
+	{
+		for (n2 = '0'; n2 <= '9'; n2++)
+		{
+			for (n3 = '0'; n3 <= '5'; n3++)
+			{
+				for (n4 = '0'; n4 <= '9'; n4++)
+				{
+					_putchar(n1);
+					_putchar(n2);
+					_putchar(':');
+					_putchar(n3);
+					_putchar(n4);
+					_putchar('\n');
+
+					if (n1 == '2' && n2 == '4' && n3 == '5' && n4 == '9')
+					{
+						goto end;
+					}
+
+				}
+
+			}
+		}
+	}
+end:
+;
+}
