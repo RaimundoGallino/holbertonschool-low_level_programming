77528c8f085f131b8971ce97d4b930f23ec9bdfc
RaimundoGallino <raigallino0@gmail.com>
2021-02-09 21:16:36 +0000

    ading the add funtion

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add - add two integers and returns the result
+ * @n: The number to print
+ * @n1: second number to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int n, int n1)
+{
+	int i = n + n1;
+
+	return (i);
+}
