142abf839158db4de9ab8474ef07ea54c2ea2498
RaimundoGallino <raigallino0@gmail.com>
2021-02-11 20:32:17 +0000

    updating files

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *  print_diagonal - print a piramide
+ *
+ * @n: number of lines
+ * Return: 1 if c is a letter.
+ *
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+
+			for (b = 1; b < size; b++)
+			{
+				_putchar(35);
+
+			}
+			_putchar(35);
+			_putchar(10);
+		}
+
+
+	}
+	else
+	{
+		_putchar(10);
+	}
+
+}
