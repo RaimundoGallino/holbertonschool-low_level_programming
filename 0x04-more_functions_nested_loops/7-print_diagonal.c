d894935d9466e9ad820d777670f263826009c886
RaimundoGallino <raigallino0@gmail.com>
2021-02-11 19:53:08 +0000

    updating file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *  print_diagonal - print a piramide
+ *
+ * @n: number of lines
+ * Return: 1 if c is a letter.
+ *
+ */
+void print_diagonal(int n)
+{
+	int sp, sk;
+
+	if (n != 0)
+	{
+		for (sk = 0; sk < n; sk++)
+		{
+
+			for (sp = 1; sp <= sk; sp++)
+			{
+				_putchar(' ');
+
+			}
+			_putchar('\\');
+			_putchar(10);
+		}
+
+
+	}
+	else
+	{
+		_putchar(10);
+	}
+
+}
