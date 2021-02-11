6ee879b551dd654c35ca137617d034f8357cefd6
RaimundoGallino <raigallino0@gmail.com>
2021-02-11 13:46:34 +0000

    multply functions and updating header

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies a * b
+ * @a: number to multiply
+ * @b: multiplier
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+	int z = a * b;
+
+	return (z);
+}
