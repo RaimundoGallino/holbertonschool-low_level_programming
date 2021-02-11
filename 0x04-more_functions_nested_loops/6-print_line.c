006273d3161d2ca2da829cdc223fac7f8ea839e8
RaimundoGallino <raigallino0@gmail.com>
2021-02-11 17:31:20 +0000

    updating files

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *  - print numbers from 0 to 9 exept 2 and 4
+ *
+ *
+ * Return: 1 if c is a letter.
+ *
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(95);
+	}
+
+	_putchar(10);
+}
