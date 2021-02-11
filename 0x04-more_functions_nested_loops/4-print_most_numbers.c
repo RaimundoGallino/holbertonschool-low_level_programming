9844f4aea367703035fa637ddf03ad4b02ca266f
RaimundoGallino <raigallino0@gmail.com>
2021-02-11 14:28:58 +0000

    adding 4 and updating header file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - print numbers from 0 to 9 exept 2 and 4
+ *
+ *
+ * Return: 1 if c is a letter.
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		if(a == '1' || a == '3')
+		{
+			a = a + 1;
+		}
+		else
+		{
+			a++;
+			_putchar(a);
+		}
+
+	}
+	_putchar(10);
+
+}
