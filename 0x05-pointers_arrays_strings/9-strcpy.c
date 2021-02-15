2ce2c21b6f570495290d96404c9be8d5447e8bbf
RaimundoGallino <raigallino0@gmail.com>
2021-02-15 15:37:21 +0000

    uploading 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *saved = dest;
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = 0;
+	return saved;
+}
