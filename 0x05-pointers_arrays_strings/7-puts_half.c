71bb2e54fbaa74d48a4914f6ee796179c3147d2f
RaimundoGallino <raigallino0@gmail.com>
2021-02-15 00:19:04 +0000

    uploading 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include "2-strlen.c"
+/**
+ * puts2 - prints every other character of a string.
+ *
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int ln = _strlen(str);
+	int ln2 = ln / 2;
+	int s = 0;
+
+	if (ln % 2 != 0)
+	{
+		ln2 = ln -1 / 2;
+	}
+
+	while (s[str + ln2] != '\0')
+	{
+		 _putchar(*str + ln2);
+		 str++;
+	}
+
+	_putchar(10);
+
+
+}
