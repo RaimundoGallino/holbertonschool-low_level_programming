57023e28ec109706d3879ac6c871d6469ee88321
RaimundoGallino <raigallino0@gmail.com>
2021-02-16 15:28:24 +0000

    uploading files

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,29 +1,21 @@
 #include "holberton.h"
+#include "2-strlen.c"
 
 /**
  * print_rev - prints a srting, in reverse..
  *
  * @*s: pointer to the string.
  */
-int _strlen(char *s)
-{
-	int str = 0;
 
-	while (s[str] != '\0')
-	{
-		str++;
-	}
-	return(str);
-}
-
-void print_rev(char *s)
+void print_rev (char *s)
 {
 	int len, i;
+
 	len = _strlen(s);
 
-	for(i=len;i>=0;i--)
+	for (i = len; i >= 0; i--)
 	{
-		_putchar(*(s+i));
+		_putchar(s[i]);
 	}
 	_putchar(10);
 }
