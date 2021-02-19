6b481ff6b493d41b05cc48a06fb16234ff5aaa79
RaimundoGallino <raigallino0@gmail.com>
2021-02-19 22:21:51 +0000

    uploading files

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 /**
- * _strcmp - compare two strings with it respectively ASCII values
- * @s1: first string to compare
- * @s2: second string to compare
- * Return: the concatenated string.
+ * reverse_array - reverse an array of integers
+ * @a: pointer to the array of integers
+ * @n: total number of the array
  */
 void reverse_array(int *a, int n)
 {
