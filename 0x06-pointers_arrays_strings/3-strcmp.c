75c0d46403a1cc90a3a9b6e8d0e5150b347ea959
RaimundoGallino <raigallino0@gmail.com>
2021-02-19 21:10:30 +0000

    uploading files

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare two strings with it respectively ASCII values
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: the concatenated string.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
