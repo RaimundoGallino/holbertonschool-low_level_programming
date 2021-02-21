a04eb6f1860df2f4017c09918751fb74ededddb8
RaimundoGallino <raigallino0@gmail.com>
2021-02-21 17:00:50 +0000

    uploading files form advanced tasks

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints the given integer.
+ *
+ * @n: the int to be printed.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n == 0)
+		_putchar('0');
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+
+}
