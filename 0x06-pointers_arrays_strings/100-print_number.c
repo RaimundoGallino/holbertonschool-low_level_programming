d320e25e5d90cdce17bc3333c67b4d1cd7a45fd3
RaimundoGallino <raigallino0@gmail.com>
2021-02-21 21:33:16 +0000

    uploading files

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -7,19 +7,21 @@
  */
 void print_number(int n)
 {
+	int i = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -n;
 	}
 
 	if (n == 0)
 		_putchar('0');
 	else
 	{
-		if (n / 10 != 0)
-			print_number(n / 10);
+		if (i / 10 != 0)
+			print_number(i / 10);
 
-		_putchar(n % 10 + '0');
+		_putchar(i % 10 + '0');
 	}
 }
