c41738639d88ea8ce4b5ef4696c26b11f6109896
RaimundoGallino <raigallino0@gmail.com>
2021-02-23 21:03:43 +0000

    999

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets the length on bytes
+ * @s: initial segment to be checked
+ * @accept: the check of bytes
+ * Return: byte number of the characters tat match
+ */
+void set_string(char **s, char *to)
+{
+	s = &to;
+
+}
