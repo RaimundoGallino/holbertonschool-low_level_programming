efd597f4567116c74f121e08c29e2ef1bf609ed8
RaimundoGallino <raigallino0@gmail.com>
2021-02-23 17:28:43 +0000

    uploading files

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character of a string
+ * @s: strint to search
+ * @c: characters to locate
+ * Return: pointer to the string
+ */
+char *_strchr(char *s, char c)
+{
+	int c1 = 0;
+	char *r = s;
+
+	while (*r != c && *r != '\0')
+	{
+		r++;
+	}
+	return (r);
+
+}
