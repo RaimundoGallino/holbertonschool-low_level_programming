633381710655b61ff2d63ccee44df0ee14f955a8
RaimundoGallino <raigallino0@gmail.com>
2021-02-23 21:14:25 +0000

    commiting 9 task

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * _strspn - gets the length on bytes
- * @s: initial segment to be checked
- * @accept: the check of bytes
+ * set_string - sets te value to the pointer to a char
+ * @s: the pointer to the pointer
+ * @to: pointer to the char
  * Return: byte number of the characters tat match
  */
 void set_string(char **s, char *to)
