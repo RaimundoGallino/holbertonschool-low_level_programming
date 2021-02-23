d00166ef906801877b0ec506a45fdf52e83f0658
RaimundoGallino <raigallino0@gmail.com>
2021-02-23 16:39:24 +0000

    uploading files

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _memcopy - copies a memory area copies n bytes from memory area src to memory area dest.
+ * @dest: adress to the memory area to be written
+ * @src: memory area to be copied
+ * @n: amount of bytes to copy
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *string = dest;
+	unsigned int c = 0;
+
+	while (c < n && src[c] != '\0')
+	{
+		dest[c] = src[c];
+		c++;
+	}
+	while (c < n)
+	{
+
+		dest[c] = '\0';
+		c++;
+	}
+
+	return (string);
+}
