0bba30fcc08c7569c04b78ecc32e01d8c808ad16
RaimundoGallino <raigallino0@gmail.com>
2021-02-24 19:00:05 +0000

    uploading files

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ * @x: base number
+ * @y: exponential number
+ * Return: the result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
