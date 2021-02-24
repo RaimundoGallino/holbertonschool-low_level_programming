a670b787c8247a4b521f6e531426fe947cd33033
RaimundoGallino <raigallino0@gmail.com>
2021-02-24 18:04:53 +0000

    uploading files

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * factorial - do the factorial of n.
+ * @n: the number to factorialized
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return(1);
+	}
+	else if ( n < 0)
+	{
+		return(-1);
+	}
+	else
+	{
+		return (n * factorial (n - 1));
+	}
+}
