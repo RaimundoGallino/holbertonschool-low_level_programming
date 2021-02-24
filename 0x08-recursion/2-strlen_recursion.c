82ea8d47310937ebf8c5825229baabd3fb3e9cec
RaimundoGallino <raigallino0@gmail.com>
2021-02-24 17:43:13 +0000

    uploading files

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - check the code for Holberton School students.
- * @s: string to print
+ * _strlen_recursion - gets the string lenght by using recursion.
+ * @s: string to check
  * Return: Always 0.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 	else
 		return (1 + _strlen_recursion(s + 1));
