df1d1de0385de853b0fd598b81cbd6d9637c42f8
RaimundoGallino <raigallino0@gmail.com>
2021-03-04 19:16:21 +0000

    uploading files

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -25,9 +25,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < (nmemb *size); i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		p[i] = 0;
 	}
-	return(p);
+	return (p);
 }
