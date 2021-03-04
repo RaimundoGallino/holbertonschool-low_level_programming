6c9f73febb986d881ca1e867ba157d776db3cd0e
RaimundoGallino <raigallino0@gmail.com>
2021-03-04 19:12:58 +0000

    adding 2nd task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - concatenates two strings
+ * @nmemb: first string to copy
+ * @size: second string to copy
+ *
+ * Return: pointer to the array.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb *size); i++)
+	{
+		p[i] = 0;
+	}
+	return(p);
+}
