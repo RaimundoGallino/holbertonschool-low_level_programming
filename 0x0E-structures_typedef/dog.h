2e5f916ef89413fbe1633a93864a8a6cef2f68e1
RaimundoGallino <raigallino0@gmail.com>
2021-03-08 17:25:01 +0000

    commiting first header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - check the code for Holberton School students.
+ * @name: name of the owner
+ * @owner: owner of the dog
+ * @age: age of the dog
+ * Return: Always 0.
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
