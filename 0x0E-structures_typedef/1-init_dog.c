7cf86d1be35aabe3dd9c1bbc8e8a5f34565f8c7d
RaimundoGallino <raigallino0@gmail.com>
2021-03-08 17:36:55 +0000

    adding task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+/**
+ * init_dog - check the code for Holberton School students.
+ * @d: the dog structure defined before
+ * @name: name of the owner
+ * @owner: owner of the dog
+ * @age: age of the dog
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *dog;
+
+	return (0);
+}
