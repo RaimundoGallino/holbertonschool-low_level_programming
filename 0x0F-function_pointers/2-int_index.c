bc40ceff3fefe1ab4204813a9d9bfcdcb4b3d07c
RaimundoGallino <raigallino0@gmail.com>
2021-03-10 18:24:34 +0000

    uploading 3rd task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,20 +11,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (cmp == NULL || array == NULL || size <= 0)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 
-		if (array[i] > cmp)
+		if (cmp(array[i]))
 		{
-			return (array[0]);
+			return (i);
 		}
-		if (size <= 0)
-		{
-			return (-1);
-		}
-		else
-		{
-			return (-1);
-		}
+
 	}
+	return (-1);
 }
