6af2d08e53139ed2944479f8e01da0da1ffabb36
RaimundoGallino <raigallino0@gmail.com>
2021-03-10 18:38:33 +0000

    updating to avoid special cases

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - holds the string given and passes it to the function (*name)
@@ -8,6 +9,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	(*f)(name);
 
 }
