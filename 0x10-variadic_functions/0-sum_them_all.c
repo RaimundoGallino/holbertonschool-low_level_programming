2ebb307f5fc3fac3bd8102f6cc286b5b84fcaa45
RaimundoGallino <raigallino0@gmail.com>
2021-03-11 16:54:28 +0000

    uploading files

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum the integers you pass
+ * @n: numbers of integers to enter
+ * Return: sum.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i = 0, sum = 0;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+
+	return (sum);
+}
