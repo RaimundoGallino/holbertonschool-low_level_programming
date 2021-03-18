c4bdaead0a2b424f1375ac676a43335c59b4f921
RaimundoGallino <raigallino0@gmail.com>
2021-03-18 20:09:07 +0000

    uploading files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * print_list - check the code for Holberton School students.
+ * @h:pointer to the headof the list
+ * Return: Always 0.
+ */
 size_t print_list(const list_t *h)
 {
 	size_t contador = 0;
 
-	
-		while (h)
+	while (h)
+	{
+		if (h->str == NULL)
 		{
-			
-			if (h->str == NULL)
-			{
-				printf("[0] (nill)\n");
-			}
-			else
-			{
-				printf("[%i] %s\n", h->len, h->str);
+			printf("[0] (nill)\n");
+		}
+		else
+		{
+			printf("[%i] %s\n", h->len, h->str);
 
-			}
-
-			contador += 1;
-
-			h = h->next;
 		}
 
-	return(contador);
-	
+		contador += 1;
+
+		h = h->next;
+}
+
+	return (contador);
 }
