7af1e021d6df0f7d2f098402e5b7aca202360092
RaimundoGallino <raigallino0@gmail.com>
2021-03-18 14:24:54 -0300

    Commiting fist files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	const list_t *contador = h;
+
+	next = NULL;
+
+	if (h)
+	{
+		while (contador->next)
+		{
+			contador = contador->next;
+			
+			if (str == NULL)
+			{
+				printf ("%s", [0] (nil));
+			}
+			printf("%s", str);
+		}
+	}
+}
