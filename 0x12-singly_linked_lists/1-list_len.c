f8fc10e36aa2d6aa7f72221f43e861cb7f9b4079
RaimundoGallino <raigallino0@gmail.com>
2021-03-18 18:17:40 -0300

    Adding 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - check the code for Holberton School students.
+ * @h:pointer to the headof the list
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t contador = 0;
+	
+	if (h)
+	{
+		while (h)
+		{
+
+			contador += 1;
+
+			h = h->next;
+		}
+}
+	
+
+	return (contador);
+}
