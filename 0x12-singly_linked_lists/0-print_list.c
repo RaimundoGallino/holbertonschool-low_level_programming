0e2055f07c6a95748ec52691b9492e896871699f
RaimundoGallino <raigallino0@gmail.com>
2021-03-18 16:45:34 -0300

    Commiting changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,20 @@ size_t print_list(const list_t *h)
 	
 		while (h)
 		{
+			
+			if (h->str == NULL)
+			{
+				printf("[0] (nill)\n");
+			}
+			printf("[%i] %s\n", h->len, h->str);
+
+			contador += 1;
+
+		
 			if (h->next != NULL)
 			{
-
-				if (h->str == NULL)
-				{
-					printf("[0] (nill)\n");
-				}
-				printf("[%i] %s\n", h->len, h->str);
-
-				contador += 1;
+				h = h->next;
 			}
-			
-			h = h->next;
 		}
 
 	return(contador);
