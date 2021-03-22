2d0f53368a23d8b3fd3c7902ccefe83923b9c558
RaimundoGallino <raigallino0@gmail.com>
2021-03-22 15:20:24 +0000

    adding 4 ex

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * free_listint - frees the nodes of a list
+ *
+ * @head:pointer to the head of the list
+ * Return: no return.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	listint_t *nextNode;
+
+	temp = head;
+
+	if (head != NULL)
+	{
+		nextNode = head->next;
+
+		while (nextNode)
+		{
+			free(temp);
+			temp = nextNode;
+			nextNode = nextNode->next;
+
+		}
+		free(temp);
+	}
+}
