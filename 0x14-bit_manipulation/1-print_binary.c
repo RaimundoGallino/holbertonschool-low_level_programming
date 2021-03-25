d14ae14a3ed41d8e16f18175ebce6e2a13af0821
RaimundoGallino <raigallino0@gmail.com>
2021-03-25 23:39:01 +0000

    uploading files

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_binary - converts binary to unsigned int
+ * @n: decimal numbers passed by a string
+ * Return: binary
+ */
+void print_binary(unsigned long int n)
+{
+
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
