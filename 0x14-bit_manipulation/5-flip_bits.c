55e3a0891478cd4221d5ee8f08c8ef718752ef96
RaimundoGallino <raigallino0@gmail.com>
2021-03-26 03:00:40 +0000

    uploading files

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - converts binary to unsigned int
+ * @n: unsigned long int
+ * @index: index of the bit you want to get
+ * Return: unsigned int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	unsigned long int num = n ^ m, con = 0;
+	unsigned int len = (sizeof(num) * 8);
+
+	while (len >= 0)
+	{
+		if (num & 1)
+			con++;
+		num = num >> 1;
+		len--;
+	}
+	return (con);
+}
