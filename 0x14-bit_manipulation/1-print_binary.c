a90e2ce62c315ccc3520c65dea5d646326bc6c8a
RaimundoGallino <raigallino0@gmail.com>
2021-03-26 00:52:53 +0000

    jaja

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,5 @@
 #include <stdio.h>
 #include "holberton.h"
-
-/**
- *print_binary_r - print binary number
- *@n: the number unsigned long int
- *
- *Return: void
- */
-void print_binary_r(unsigned long int n)
-{
-	unsigned long int ram;
-
-	if (n == 0)
-		return;
-
-	ram = n & 1;
-
-	n = n >> 1;
-	print_binary_r(n);
-
-	_putchar(ram + '0');
-}
-
-
 /**
  *print_binary - print binary number
  *@n: the number unsigned
@@ -31,11 +8,9 @@ void print_binary_r(unsigned long int n)
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
 
-	print_binary_r(n);
+	_putchar((n & 1) + '0');
+
 }
