67a4d6a18cb138059d7ad78dc5bf5f6d072f9323
RaimundoGallino <raigallino0@gmail.com>
2021-03-26 00:41:00 +0000

    uploading files

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include "holberton.h"
+
 /**
- * print_binary - converts binary to unsigned int
- * @n: decimal numbers passed by a string
- * Return: binary
+ *print_binary_r - print binary number
+ *@n: the number unsigned long int
+ *
+ *Return: void
+ */
+void print_binary_r(unsigned long int n)
+{
+	unsigned long int ram;
+
+	if (n == 0)
+		return;
+
+	ram = n & 1;
+
+	n = n >> 1;
+	print_binary_r(n);
+
+	_putchar(ram + '0');
+}
+
+
+/**
+ *print_binary - print binary number
+ *@n: the number unsigned
+ *
+ *Return: void
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 1)
-		print_binary(n >> 1);
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-	_putchar((n & 1) + '0');
+	print_binary_r(n);
 }
