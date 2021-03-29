70d830cc03c3b7c9e1096ef9182d972e5e405fd6
RaimundoGallino <raigallino0@gmail.com>
2021-03-29 22:32:49 +0000

    uploading files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,9 +20,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t w = 0;
 
 	if (!filename)
-	{
 		return (-1);
-	}
+
+	if (!text_content)
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
