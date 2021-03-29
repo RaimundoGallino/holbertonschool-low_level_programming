60a2f71632a60755a659d1e5c49d6aa1bc87a299
RaimundoGallino <raigallino0@gmail.com>
2021-03-29 19:56:37 +0000

    uploading files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,15 +19,13 @@ int create_file(const char *filename, char *text_content)
 	int fd = 0, i = 0;
 
 	if (!filename)
-	{
 		return (-1);
-	}
+	if (!text_content)
+		text_context = "";
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
-	{
 		return (-1);
-	}
 
 	for (; text_content[i] != '\0';)
 	{
