145ec67b49fb4181e598f7a030b7b7a183070285
RaimundoGallino <raigallino0@gmail.com>
2021-03-30 02:59:16 +0000

    uploading files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,48 +7,45 @@
 #include "holberton.h"
 
 /**
- * append_text_to_file - append content to an existing file
- * @filename1: name of the file
- * @filename2:text content to be written inside the file
+ * main - program that copies the content of a file to another file
+ * @av:name of the file
+ * @ac:text content to be written inside the file
  * Return:number of letters it could read and print
- */
-
-/*
- * int copy_file_content(const char *filename1, const char *filename2)
- * {
- * }
  */
 
 int main(int ac, char **av)
 {
 
-	const char *file1, *file2;
-	int fd1, fd2, i = 0;
+	const char *filef, *filet;
+	int fd1, fd2, r = 0;
 	ssize_t w;
+	char buff[1024];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
 
-	file1 = av[1];
-	file2 = av[2];
+	filef = av[1];
+	filet = av[2];
 
-	fd1 = open(file1, O_RDONLY | O_TRUNC);
-	fd2 = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd1 = open(filef, O_RDONLY);
+	fd2 = open(filet, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
 	if (fd1 == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	while (*file1 != '\0')
-	{
-		i++;
-	}
+	r = read(fd1, buff, 1024);
 
-	w = write(fd2, file1, i);
+	w = write(fd2, buff, r);
+
 	if (w == -1)
 		return (-1);
 
 	close(fd1);
 	close(fd2);
+	return (0);
 }
