a9d8f8e609a032e15860a3a0f2cf05d96e8ee4f4
RaimundoGallino <raigallino0@gmail.com>
2021-03-30 03:10:37 +0000

    uploading files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,6 @@
 
 int main(int ac, char **av)
 {
-
 	const char *filef, *filet;
 	int fd1, fd2, r = 0;
 	ssize_t w;
@@ -26,28 +25,22 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	filef = av[1];
 	filet = av[2];
-
 	fd1 = open(filef, O_RDONLY);
 	fd2 = open(filet, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	r = read(fd1, buff, 1024);
-
 	w = write(fd2, buff, r);
-
 	if (w == -1 || r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
 	close(fd1);
 	if (close(fd1) == -1)
 	{
@@ -57,7 +50,7 @@ int main(int ac, char **av)
 	close(fd2);
 	if (close(fd2) == -1)
 	{
-                dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
 		exit(100);
 	}
 	return (0);
