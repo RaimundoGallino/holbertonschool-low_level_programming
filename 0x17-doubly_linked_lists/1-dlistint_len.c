97791646ced029ebb1c59e375fb34592d7c8fc9e
RaimundoGallino <raigallino0@gmail.com>
2021-04-30 12:02:24 -0300

    Uploading files

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+
+    const dlistint_t *current;
+    size_t count = 0;
+
+    current = h;
+
+    while (current->next != NULL)
+    {
+        current = current->next; 
+        count++;
+    }
+
+    while (current->prev != NULL) 
+    {
+        current = current->prev;
+        count++;
+    }
+    return(count);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
