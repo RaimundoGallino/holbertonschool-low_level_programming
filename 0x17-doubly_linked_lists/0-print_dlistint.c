62eff269f9e8e3fcda90f34539f08cd57740e0c6
RaimundoGallino <raigallino0@gmail.com>
2021-04-30 11:05:18 -0300

    Uploading files

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,9 +5,9 @@ size_t print_dlistint(const dlistint_t *h)
     const dlistint_t *current;
     size_t count = 0;
 
-    current = h;
+    current = h->next;
 
-        while (current->next != NULL)
+        while (current != NULL)
         {
             printf("%i\n",current->n);
             current = current->next;
