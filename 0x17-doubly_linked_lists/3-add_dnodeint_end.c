fed2deabbe0b5f87ba614a8bee182013a61b59b1
RaimundoGallino <raigallino0@gmail.com>
2021-04-30 16:16:17 -0300

    Uploading files

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,24 +14,24 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
+    new->n = n;
+    new->next = NULL;
     
     if (head != NULL)
     {
-        new->next = NULL;
-        new->prev = NULL;
-        new->n = n;
+        new->prev = NULL;   
         *head = new;
     }
     else
     {
         temp = *head;
-        while (temp != NULL)  
+        while (temp->next != NULL)  
         {     
             temp = temp->next;
         }
         temp->next = new;
         new ->prev = temp;
-        new->next = NULL;
     }
     return (new);
 }
