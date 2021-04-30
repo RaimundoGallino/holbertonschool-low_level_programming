350559636f909fbe5cd1ea85b6b74f70b5aa5afa
RaimundoGallino <raigallino0@gmail.com>
2021-04-30 11:06:19 -0300

    Uploading fiels

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,9 +5,9 @@ size_t print_dlistint(const dlistint_t *h)
     const dlistint_t *current;
     size_t count = 0;
 
-    current = h->next;
+    current = h;
 
-        while (current != NULL)
+        while (current->next != NULL)
         {
             printf("%i\n",current->n);
             current = current->next;
