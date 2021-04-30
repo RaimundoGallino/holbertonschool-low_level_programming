7a71c6c0e6deb5a7cb6b257c941e2a1a9ab793b8
RaimundoGallino <raigallino0@gmail.com>
2021-04-30 13:22:02 -0300

    Uploading files

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
         new->next = NULL;
         new->prev = NULL;
         new->n = n;
-        head = new;
+        *head = new;
     }
     else
     {
