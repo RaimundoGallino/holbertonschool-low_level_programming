ab3e64c6bb170f18a09992c8d34ed0b6f236417a
RaimundoGallino <raigallino0@gmail.com>
2021-05-02 16:41:13 -0300

    Uploading files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 /**
+ * dlistint_len - prints the lenght of the list
+ * @h: head of the list
+ * Return: Lenght of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+
+	const dlistint_t *current;
+	size_t count = 0;
+
+	current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
+/**
  * insert_dnodeint_at_index - adds a node to the index position given
  * @h: double pointer to the list
  * @idx: index number of the node
