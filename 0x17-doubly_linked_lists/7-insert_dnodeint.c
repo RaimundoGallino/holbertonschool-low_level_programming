69be976e753e9bf920c08b08cc7e07f2d238b1d3
RaimundoGallino <raigallino0@gmail.com>
2021-05-02 16:39:57 -0300

    Uploading files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(&(*h), n));
-	if ((unsigned int)dlistint_len(&h) == idx)
-		return (add_dnodeint_en(&(*h), n));
+	if ((unsigned int)dlistint_len(*h) == idx)
+		return (add_dnodeint_end(&(*h), n));
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
