f7dcad1323caf8e494c4e6aba68e90dd31b56578
RaimundoGallino <raigallino0@gmail.com>
2021-05-27 20:26:12 -0300

    adding firts files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,9 +9,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     unsigned long int i;
+    
     hash_table_t *new_table = malloc(sizeof(hash_table_t));
     if (!new_table)
         return(NULL);
+    
     new_table->array = malloc(sizeof(hash_node_t **) * size);
     if (!new_table->array)
         return(NULL);
