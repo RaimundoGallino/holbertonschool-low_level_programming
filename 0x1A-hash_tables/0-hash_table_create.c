f6f3449b31c153617d154b4de9327d7de714a60f
RaimundoGallino <raigallino0@gmail.com>
2021-05-27 17:48:51 -0300

    adding firts files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @size: string used to generate hash value
+ *
+ * Return: hash value
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    unsigned long int i;
+    hash_table_t *new_table = malloc(sizeof(hash_table_t));
+    if (!new_table)
+        return(NULL);
+    new_table->array = malloc(sizeof(hash_node_t **) * size);
+    if (!new_table->array)
+        return(NULL);
+
+    new_table->size = size;
+
+    for (i = 0; i < size; i++)
+    {
+        new_table->array[i] = NULL;
+    }
+
+    return new_table;
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @key: string used to generate hash value
+ * @size: size of the list
+ *
+ * Return: hash value
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    
+
+}
