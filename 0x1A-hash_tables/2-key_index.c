f3a4abff4efd86437e6a79aa2b0ecbe69f6ddb23
RaimundoGallino <raigallino0@gmail.com>
2021-05-27 20:51:11 -0300

    adding firts files

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,20 +10,13 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 
-    unsigned int i;
-    hash_table_t *new_table = malloc(sizeof(hash_table_t));
-    if (!new_table)
-        return(NULL);
-    new_table->array = malloc(sizeof(hash_node_t **) * size);
-    if (!new_table->array)
-        return(NULL);
+    unsigned int index;
+    const unsigned char *hash;
 
-    for (i = 0; i < size; i++)
+    if (!key)
     {
-        if (strcmp(new_table->array[i], key) == 0)
-        {
-            return hash_djb2(new_table->array[i]);
-        }
+        hash = hash_djb2(key);
+        index = hash % size;
     }
 
 }
