d503fe20a133b9aa5fdb8a6ed20b5f88c1c7db11
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 17:37:30 -0300

    / adding files

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,10 +22,10 @@ void hash_table_print(const hash_table_t *ht)
 	    tmp = ht->array[i];
         while (tmp)
         {
-            printf("'%s': '%s'",tmp->key, tmp->value);
-
             if (flag == 1)
                 printf(", ");
+            printf("'%s': '%s'",tmp->key, tmp->value);
+
             tmp = tmp->next;
             flag = 1;
         }
