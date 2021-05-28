796bd99ab645fc18e304d4826d2f8250b6dce5f7
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 17:38:30 -0300

    / adding files

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,25 +10,25 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *tmp;
-    unsigned long int i, flag = 0;
+	unsigned long int i, flag = 0;
 
 
 	if (!ht)
 		return;
-    printf("{");
+	printf("{");
 
-    for (i = 0; i < ht->size; i++)
-    {
-	    tmp = ht->array[i];
-        while (tmp)
-        {
-            if (flag == 1)
-                printf(", ");
-            printf("'%s': '%s'",tmp->key, tmp->value);
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'",tmp->key, tmp->value);
 
-            tmp = tmp->next;
-            flag = 1;
-        }
-    }
-    printf("}\n");
+			tmp = tmp->next;
+			flag = 1;
+		}
+	}
+	printf("}\n");
 }
