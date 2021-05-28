b2d58663a3b5bbfef635d755fe63704890034f38
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 15:41:01 -0300

    / adding files

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(tmp->key, key) == 0)
 		{
 			printf("%s\n",tmp->key);
-			strcpy(tmp->value, value);
+			tmp->value = (char *)value;
 			return (1);
 		}
 		tmp = tmp->next;
