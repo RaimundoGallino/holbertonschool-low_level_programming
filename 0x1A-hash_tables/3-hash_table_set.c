3aee019073aaa0170c2b4446a9f0ea6926a11e12
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 15:19:31 -0300

    adding files

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,6 +34,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
 
 	strcpy(new_node->key, key);
 	strcpy(new_node->value, value);
