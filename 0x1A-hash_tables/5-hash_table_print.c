161f92e6ae61bbcdfab2586b6f4cd543f2e36af7
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 16:59:45 -0300

    / adding files

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - implementation of the djb2 algorithm
+ * @ht: hash table
+ * @key: string used to generate hash value
+ *
+ * Return: hash value
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int index;
+
+	if (!ht || !ht->array)
+		return;
+	tmp = ht;
+
+	while (tmp)
+	{
+        printf("'%s': '%s'",tmp->key, tmp->value);
+
+        if (tmp->next)
+            printf(",");
+        tmp = tmp->next;
+	}
+}
