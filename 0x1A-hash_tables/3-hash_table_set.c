165a44cf83f92e3a5632c28a280117777315d7d2
RaimundoGallino <raigallino0@gmail.com>
2021-05-27 22:12:36 -0300

    adding files

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
     hash_node_t *new_node;
     const char *val = strdup(value);
-    const unsigned char *kk; strdup(key);
+    const unsigned char *kk;
     unsigned long int index;
 
     if (value == NULL)
