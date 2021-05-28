92371591590ac592f31e6585855a631dfe1752a5
RaimundoGallino <raigallino0@gmail.com>
2021-05-27 23:13:27 -0300

    adding files

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,8 +21,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         if (strlen(key) != 0)
             return (0);
     }
-    index = key_index(&key, ht->size);
-    
+    index = key_index((const unsigned char)key, ht->size);
+
     if (ht->array[index] == NULL)
     {
         new_node = malloc(sizeof(hash_node_t));
@@ -33,5 +33,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     }
     else
         return (0);
-
 }
