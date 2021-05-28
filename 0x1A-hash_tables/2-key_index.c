4be7d14e25462798c1e4663f734e66488b5b7df6
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 11:20:04 -0300

    adding files

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_djb2 - implementation of the djb2 algorithm
+ * key_index - implementation of the djb2 algorithm
  * @key: string used to generate hash value
  * @size: size of the list
  *
