58f364fdcb5a0499692ab5b107672709097d48e9
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 18:42:41 -0300

    adding files

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - implementation of the djb2 algorithm
+ * @ht: hash table
+ *
+ * Return: hash value
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			free(tmp->key);
+            free(tmp->value);
+			tmp = tmp->next;
+		}
+        free(ht->array[i]);
+	}
+    free(tmp);
+    free(ht);
+}
