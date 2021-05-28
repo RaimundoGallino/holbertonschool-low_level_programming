2a2e3c73c1bec3b15076dbc9866b04649ffc2b50
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 12:43:11 -0300

    adding files

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,9 +25,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[index] == NULL)
 	{
+		new_node = malloc(sizeof(hash_node_t));
+
 		if (strcmp(new_node->key, key) != 0)
 		{
-			new_node = malloc(sizeof(hash_node_t));
 			strcpy(new_node->value, value);
 			strcpy(new_node->key, key);
 			new_node->next = NULL;
@@ -35,7 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		else
 		{
-			new_node = malloc(sizeof(hash_node_t));
 			strcpy(new_node->value, value);
 			new_node->next = NULL;
 			return (1);
