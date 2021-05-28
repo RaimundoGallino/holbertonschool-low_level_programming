f4c3ab370cba34433cd36a12ae521d431097cd22
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 20:05:21 -0300

    adding files

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, (char *)key) == 0)
 		{
-			tmp->value = (char *)value;
+			tmp->value = strdup(value);
 			return (1);
 		}
 		tmp = tmp->next;
