4aa9bb9553af24d6790cf1c7477f3ac7eec65694
RaimundoGallino <raigallino0@gmail.com>
2021-05-27 22:02:32 -0300

    adding files

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @key: string used to generate hash value
+ * @size: size of the list
+ *
+ * Return: hash value
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+    hash_node_t *new_node;
+    const char *val = strdup(value);
+
+    unsigned long int index, i;
+
+    if (value == NULL)
+        return (0);
+    if (key != NULL)
+    {
+        if (strlen(key) != 0)
+            return (0);
+    }
+
+    index = key_index(key, ht->size);
+    
+    if (ht->array[index] == NULL)
+    {
+        new_node = malloc(sizeof(hash_node_t));
+        strcopy(new_node->value, val);
+        strcopy(new_node->key, key);
+        new_node->next = NULL;
+        return (1);
+    }
+    else
+        return (0);
+
+}
