211ce5bd65288f03cb81f150a5e46398642ba4be
RaimundoGallino <raigallino0@gmail.com>
2021-05-28 11:14:44 -0300

    adding files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,13 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
-		return(NULL);
+		return (NULL);
 
 	new_table->array = malloc(sizeof(hash_node_t **) * size);
 	if (!new_table->array)
 	{
 		free(new_table);
-		return(NULL);
+		return (NULL);
 	}
 
 	new_table->size = size;
@@ -28,5 +28,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new_table->array[i] = NULL;
 	}
 
-	return new_table;
+	return (new_table);
 }
