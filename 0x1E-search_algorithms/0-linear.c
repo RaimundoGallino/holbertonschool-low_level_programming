67cc38747f916321dccf31815ed03b6f65bc7210
RaimundoGallino <raigallino0@gmail.com>
2021-09-01 10:58:36 -0300

    Uploading files

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,16 +9,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    size_t i;
- 
-    for(i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (value);
-    }
-    return (-1);
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (value);
+	}
+	return (-1);
 }
