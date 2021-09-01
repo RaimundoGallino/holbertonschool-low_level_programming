d8f60c9bd3e94742503afc94938b0c9d55bcbd9f
RaimundoGallino <raigallino0@gmail.com>
2021-09-01 17:35:44 -0300

    Uploading files

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (start <= end)
 	{
-		middle = start + (end - start ) / 2;
+		middle = start + (end - start) / 2;
 		printf("Searching in array: ");
 		for (i = start; i < end; i++)
 		{
